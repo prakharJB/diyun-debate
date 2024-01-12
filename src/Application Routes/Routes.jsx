f03d2db8dcb01fdc222938091ce06f6c04ecc1ac
rashmidubey74 <jmbliss20@gmail.com>
2024-01-12 17:38:54 +0530

    AllUser

--- src/Application Routes/Routes.jsx
+++ src/Application Routes/Routes.jsx
@@ -14,6 +14,8 @@ import UserSettings from "../Screens/UserScreen/UserSettings/UserSettings";
 import PrivateRoutes from "./PrivateRoute";
 import UserProfile from "../Screens/UserScreen/UserProfile/UserProfile";
 import NewBg from "../Screens/UserScreen/ExploreBg/ExploreBg";
+import AdminDashboard from "../Screens/Admin/AdminDashboard/AdminDashboard";
+import AllUser from "../Screens/Admin/AllUser/AllUser";
 
 function AppRoutes() {
   return (
@@ -46,6 +48,7 @@ function AppRoutes() {
           <Route path="/terms" element={<Terms />}></Route>
           <Route path="/user-profile" element={<></>}></Route>
           <Route path="/user-setting" element={<UserSettings />}></Route>
+          <Route path="/admin-dashboard" element={< AllUser />} ></Route>
         </Routes>
       </Router>
     </div>
